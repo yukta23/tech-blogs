c6d3c7109ad62aea5531783ef24b4d917402b2ac
ykushwaha <ykushwaha@twilio.com>
2022-09-19 10:01:32 +0530

    Refactoring

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 export const Footer = () => (
     <footer className="footer">
-        {/* <p>&copy; yukta23 {new Date().getFullYear()}</p> */}
+        <p className="footer-text">&copy; yukta23 {new Date().getFullYear()}</p>
     </footer>
 )
